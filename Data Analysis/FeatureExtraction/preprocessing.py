9291af1d6dfcbd334100d8f2f1beeb0dc089f09b
Sarah Wu <swu82@jh.edu>
2024-02-04 14:39:35 -0500

    slicing done

--- Data Analysis/FeatureExtraction/preprocessing.py
+++ Data Analysis/FeatureExtraction/preprocessing.py
@@ -10,7 +10,7 @@ x_data = pd.read_csv('./Patient9_Data_Set1.csv')
 sbs_score = pd.read_excel('./Patient_9_SBS_Scores.xlsx', header=2, usecols='A:C')
 #%%
 sbs_score['dts'] = pd.to_datetime(sbs_score['Time_uniform'], format='%m/%d/%Y %H:%M:%S %p')
-x_data['dts'] = pd.to_datetime(x_data['time'], format='mixed')
+x_data['dts'] = pd.to_datetime(x_data['time'], format='%Y-%m-%d %H:%M:%S.%f')
 #%%
 acc = x_data[['X', 'Y', 'Z']]
 mag = np.linalg.norm(acc, axis=1)
@@ -27,7 +27,7 @@ for index, row in sbs_score.iterrows():
     conditions = (x_data['dts'] > sbs_time_start) & (x_data['dts'] < sbs_time_end)
     x_mag = x_data[conditions]['mag'].to_numpy()
     if x_mag.shape[0] > 0:
-        x_.append(x_mag[:59899])
+        x_.append(x_mag[:59899]) #min length of all x_mag
         y.append(row['SBS'])
     
 
@@ -36,6 +36,5 @@ x_mag_data = np.vstack(x_)
 sbs_data = np.array(y)
 
 # %%
-x_train_data = np.concatenate([np.vstack([tsfel.time_series_features_extractor(cfg_file, X_train[i, k, :], fs = fs, verbose = 0).to_numpy() for i in range(X_train.shape[0])])[:, :, None] for k in range(X_train.shape[1])], axis = 2)
-
+savemat('pt9_win5_5.mat', dict([('x_mag', x_mag_data), ('sbs', sbs_data)]))
 # %%
