fda1e8e024d210472e59d1f4ad61b306c0b250d8
Sarah Wu <swu82@jh.edu>
2024-02-13 22:32:45 -0800

    linear model

--- JHU SAFE/linear_model/preprocessing.py
+++ JHU SAFE/linear_model/preprocessing.py
@@ -9,41 +9,48 @@ import torch.optim as optim
 from sklearn import preprocessing
 from sklearn.linear_model import LinearRegression
 import tsfel
+from sklearn.model_selection import KFold
+from sklearn import metrics
+
 # %%
-X_train = loadmat('../DONOTPUSH/data.mat')['X_train']
-y_train = np.round(loadmat('../DONOTPUSH/data.mat')['y_train'])  
-X_test = loadmat('../DONOTPUSH/data.mat')['X_test']   
-y_test = np.round(loadmat('../DONOTPUSH/data.mat')['y_test'])
-# %%
-sns.histplot(y_test.ravel())
-sns.histplot(y_train.ravel())
+X = loadmat('../DONOTPUSH/data.mat')['X']
+y = np.round(loadmat('../DONOTPUSH/data.mat')['y'])  
+
+#%%
+kf = KFold(n_splits=10, shuffle = True)
+
 # %%
 # tsfel (https://tsfel.readthedocs.io/en/latest/)
 cfg_file = tsfel.get_features_by_domain()
+del cfg_file['spectral']["LPCC"]
+del cfg_file["spectral"]["MFCC"]
+del cfg_file["spectral"]["Spectral roll-on"]
 fs = 1/60
 
 # %%
-x_train_data = np.concatenate([np.vstack([tsfel.time_series_features_extractor(cfg_file, X_train[i, k, :], fs = fs, verbose = 0).to_numpy() for i in range(X_train.shape[0])])[:, :, None] for k in range(X_train.shape[1])], axis = 2)
-x_test_data = np.concatenate([np.vstack([tsfel.time_series_features_extractor(cfg_file, X_test[i, k, :], fs = fs, verbose = 0).to_numpy() for i in range(X_test.shape[0])])[:, :, None] for k in range(X_test.shape[1])], axis = 2)
+x_features = np.concatenate([np.vstack([tsfel.time_series_features_extractor(cfg_file, X[i, k, :], fs = fs, verbose = 0).to_numpy() for i in range(X.shape[0])])[:, :, None] for k in range(X.shape[1]-1)], axis = 2)
 
 # %%
-x_train_data = x_train_data.reshape(x_train_data.shape[0], -1)
-x_test_data = x_test_data.reshape(x_test_data.shape[0], -1)
-# %%
-# normalizing input features
-scaler = preprocessing.StandardScaler()
-x_train_data = scaler.fit_transform(x_train_data)
-x_test_data = scaler.transform(x_test_data)
-# %%
-x_total = np.concatenate((x_train_data, x_test_data), axis = 0)
-x_total = x_total[:, ~np.any(np.isnan(x_total), axis = 0)]
-# %%
-x_train = x_total[:x_train_data.shape[0], :]
-x_test = x_total[x_train_data.shape[0]:, :]
+x_features = x_features.reshape(x_features.shape[0], -1)
+X = np.concatenate((x_features, X[:, 3, :].mean(axis = 1)[:, None]), axis=1)
 #%%
-y_train = y_train.reshape(-1, 1)
-y_test = y_test.reshape(-1, 1) 
-# %%
-filename = "fold1.mat"
-savemat(filename, {'X_train':x_train, 'y_train':y_train, 'X_test':x_test, 'y_test':y_test})
+y = np.squeeze(y[:])
+#%%
+for i, (train_idx, test_idx) in enumerate(kf.split(X)):
+    x_train_data = X[train_idx, :]
+    y_train_data = y[train_idx]
+    x_test_data = X[test_idx, :]
+    y_test_data = y[test_idx]
+    # normalizing input features
+    scaler = preprocessing.StandardScaler()
+    x_train_data = scaler.fit_transform(x_train_data)
+    x_test_data = scaler.transform(x_test_data)
+
+    x_total = np.concatenate((x_train_data, x_test_data), axis = 0)
+    x_total = x_total[:, ~np.any(np.isnan(x_total), axis = 0)]
+    x_train = x_total[:x_train_data.shape[0], :]
+    x_test = x_total[x_train_data.shape[0]:, :]
+
+    filename = f"fold{i}.mat"
+    savemat(filename, {'X_train':x_train, 'y_train':y_train_data, 'X_test':x_test, 'y_test':y_test_data})
 # %%
