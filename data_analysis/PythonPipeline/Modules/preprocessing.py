07483373fe33db787c4c8930a0bc39b6b2f6eddb
Sidharth Raghavan <sidharth.raghavan@gmail.com>
2024-02-26 13:48:59 -0500

    minor edits

--- data_analysis/PythonPipeline/Modules/preprocessing.py
+++ data_analysis/PythonPipeline/Modules/preprocessing.py
@@ -10,10 +10,10 @@ import sysconfig;
 #Where python looks for libraries
 print(sysconfig.get_paths()["purelib"])
 #%%
-x_data = pd.read_csv('./Patient11_FullSBS.csv')
+x_data = pd.read_csv('./Patient9_FullSBS.csv')
 #x_data = pd.read_csv('./Users/jakes/Documents/DT 6 Analysis/PythonCode/Patient9_Data_Set1')
 #%%
-sbs_score = pd.read_excel('./Patient11_SBS_Scores.xlsx', header=2, usecols='A:C')
+sbs_score = pd.read_excel('./Patient9_SBS_Scores.xlsx', header=2, usecols='A:C')
 #%%
 sbs_score['dts'] = pd.to_datetime(sbs_score['Time_uniform'], format='%m/%d/%Y %H:%M:%S %p')
 x_data['dts'] = pd.to_datetime(x_data['time'], format='mixed')
@@ -36,10 +36,14 @@ for index, row in sbs_score.iterrows():
         x_.append(x_mag[:59899]) #min length of all x_mag
         y.append(row['SBS'])
 #%%
+# Now, you can proceed with your operation
+mask = (0.9 <= x_) & (x_ <= 1.1)
+x_[mask] = 1
+
 x_mag_data = np.vstack(x_)
 sbs_data = np.array(y)
 # %%
 os.chdir(r'C:\Users\sidha\OneDrive\Sid Stuff\PROJECTS\iMEDS Design Team\Data Analysis\PedAccel\data_analysis\PythonPipeline\PatientData\Patient9')
 # os.chdir(r'C:\Users\jakes\Documents\DT 6 Analysis\PythonCode\PedAccel\Data Analysis\PythonPipeline\PatientData\Patient9')
-savemat('Patient11_5MIN_DSW_AllSBS.mat', dict([('x_mag', x_mag_data), ('sbs', sbs_data)]))
+savemat('Patient9_2_5MIN_DSW_AllSBS.mat', dict([('x_mag', x_mag_data), ('sbs', sbs_data)]))
 # %%
