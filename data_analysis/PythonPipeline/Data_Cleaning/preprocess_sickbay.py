00cb7e530470949630265494662e32e18d8db4b5
Jake <jakesamuelshoffmann@gmail.com>
2024-04-09 01:14:03 -0400

    preprocessing with vitals works

--- data_analysis/PythonPipeline/Data_Cleaning/preprocess_sickbay.py
+++ data_analysis/PythonPipeline/Data_Cleaning/preprocess_sickbay.py
@@ -1,35 +1,12 @@
 import pandas as pd
 import numpy as np
-from pygt3x.reader import FileReader 
 import os
 from scipy.io import savemat
 from functools import reduce
 from scipy.io import loadmat
 
-def check_window(vitals_list, vitals_window, sbs, vital, row):
-    if not vitals_window.empty:  # Check if any heart rate values are found in the window
-        sbs.append(row['SBS'])
-                    
-        # Calculate the relative time within the window
-        vitals_window['dts'] = vitals_window['dts'] - row['start_time']
-                    
-        # Append heart rate values and corresponding relative time to the list
-        vitals_list.append(vitals_window[vital].tolist())
-        print(len(vitals_window[vital]))
-        sbs = np.array(sbs)
-    else:
-        print(f'No matching {vital} data for SBS recording at ', row['dts'])
-
 def load_from_excel(sbs_filepath, to_numpy=False, verbose=False):
-    # TODO: define a data model and ingest all the metadata we care about at once elegantly
-    '''
-    Load data from Excel file
-
-    WARNING! This function assumes that 
-    1. the header is on the 3rd row. 
-    2. the excel contains a column 'SBS'
-    This is very fragile. We should consider the regularity of our excel formatting. 
-    '''
+    # Load data from Excel file
     df = pd.read_excel(sbs_filepath, header=0)
     col_names = df.columns.values.tolist()
     if 'SBS' not in col_names:
@@ -42,7 +19,6 @@ def load_from_excel(sbs_filepath, to_numpy=False, verbose=False):
         if verbose:
             print(array.shape)
         return array, col_names
-
     return df, col_names
 
 def load_segment_sickbay(data_dir, window_size=10, lead_time=5):
@@ -79,59 +55,68 @@ def load_segment_sickbay(data_dir, window_size=10, lead_time=5):
             vitals_data['blood_pressure_mean'] = vitals_data['blood_pressure_mean'].flatten()  # Flatten heart rate array
             vitals_data['blood_pressure_diastolic'] = vitals_data['blood_pressure_diastolic'].flatten()  # Flatten heart rate array
 
-
             # Create a DataFrame from the dictionary
-            heart_rate_df = pd.DataFrame({'dts': vitals_data['dts'], 'heart_rate': vitals_data['heart_rate']})
-            SpO2_df = pd.DataFrame({'dts': vitals_data['dts'], 'SpO2': vitals_data['SpO2']})
-            respiratory_rate_df = pd.DataFrame({'dts': vitals_data['dts'], 'respiratory_rate': vitals_data['respiratory_rate']})
-            blood_pressure_systolic_df = pd.DataFrame({'dts': vitals_data['dts'], 'blood_pressure_systolic': vitals_data['blood_pressure_systolic']})
-            blood_pressure_mean_df = pd.DataFrame({'dts': vitals_data['dts'], 'blood_pressure_mean': vitals_data['blood_pressure_mean']})
-            blood_pressure_diastolic_df = pd.DataFrame({'dts': vitals_data['dts'], 'blood_pressure_diastolic': vitals_data['blood_pressure_diastolic']})
-
-            hr_sbs = []
-            SpO2_sbs = []
-            rr_sbs = []
-            bps_sbs = []
-            bpm_sbs = []
-            bpd_sbs = []
-            heart_rates = []
-            SpO2 = []
-            respiratory_rate = []
-            blood_pressure_systolic = []
-            blood_pressure_mean = []
-            blood_pressure_diastolic = []
+            vitals_data_df = pd.DataFrame({'dts': vitals_data['dts'], 'heart_rate': vitals_data['heart_rate'], 'SpO2': vitals_data['SpO2'], 'respiratory_rate': vitals_data['respiratory_rate']
+                                           , 'blood_pressure_systolic': vitals_data['blood_pressure_systolic'], 'blood_pressure_mean': vitals_data['blood_pressure_mean']
+                                           , 'blood_pressure_diastolic': vitals_data['blood_pressure_diastolic']})
+        
+            windows = []
+            sbs = []
             
             for i, row in epic_data.iterrows():
                 # Define the time window
-                start_time = row['start_time'] - pd.Timedelta(minutes=10)
-                end_time = row['end_time'] + pd.Timedelta(minutes=5)
+                start_time = row['start_time'] - pd.Timedelta(minutes=lead_time)
+                end_time = row['end_time'] + pd.Timedelta(minutes=lead_time)
 
                 # Filter heart rate data within the time window
-                hr_in_window = heart_rate_df[(heart_rate_df['dts'] > start_time) & (heart_rate_df['dts'] < end_time)]
-                SpO2_in_window = SpO2_df[(SpO2_df['dts'] > start_time) & (SpO2_df['dts'] < end_time)]
-                rr_in_window = respiratory_rate_df[(heart_rate_df['dts'] > start_time) & (respiratory_rate_df['dts'] < end_time)]
-                bps_in_window = blood_pressure_systolic_df[(blood_pressure_systolic_df['dts'] > start_time) & (blood_pressure_systolic_df['dts'] < end_time)]
-                bpm_in_window = blood_pressure_mean_df[(blood_pressure_mean_df['dts'] > start_time) & (blood_pressure_mean_df['dts'] < end_time)]                
-                bpd_in_window = blood_pressure_diastolic_df[(blood_pressure_diastolic_df['dts'] > start_time) & (blood_pressure_diastolic_df['dts'] < end_time)]
+                in_window = vitals_data_df[(vitals_data_df['dts'] >= start_time) & (vitals_data_df['dts'] <= end_time)]
+                if not in_window.empty:  # Check if any data values are found in the window
+                    sbs.append(row['SBS'])
 
-                check_window(heart_rates, hr_in_window, hr_sbs, 'heart_rate', row)
-                check_window(SpO2, SpO2_in_window, SpO2_sbs, 'SpO2', row)
-                check_window(respiratory_rate, rr_in_window, rr_sbs, 'respiratory_rate', row)
-                check_window(blood_pressure_systolic, bps_in_window, bps_sbs, 'blood_pressure_systolic', row)
-                check_window(blood_pressure_mean, bpm_in_window, bpm_sbs, 'blood_pressure_mean', row)
-                check_window(blood_pressure_diastolic, bpd_in_window, bpd_sbs, 'blood_pressure_diastolic', row)
+                    # Calculate the relative time within the window
+                    in_window['dts'] = in_window['dts'] - row['start_time']
 
-        
-            # Further processing and saving...
+                    # Rename columns with unique names based on iteration index 'i'
+                    rename_dict = {
+                        'heart_rate': f'heart_rate_{i}',
+                        'SpO2': f'SpO2_{i}',
+                        'respiratory_rate': f'respiratory_rate_{i}',
+                        'blood_pressure_systolic': f'blood_pressure_systolic_{i}',
+                        'blood_pressure_mean': f'blood_pressure_mean_{i}',
+                        'blood_pressure_diastolic': f'blood_pressure_diastolic_{i}'
+                    }
+                    in_window.rename(columns=rename_dict, inplace=True)
+
+                    # Append data to the list
+                    windows.append(in_window)
+
+            # Convert sbs to a numpy array
+            sbs = np.array(sbs)
             
-            filename = f'{patient}_SICKBAY_{lead_time}MIN_{window_size - lead_time}MIN.mat'
-            save_file = os.path.join(patient_dir, filename)
-            savemat(save_file, {'heart_rate_sbs': hr_sbs, 'heart_rate': heart_rates, 'SpO2_sbs': SpO2_sbs, 'SpO2': SpO2,'respiratory_rate_sbs': rr_sbs, 
-                                'respiratory_rate': respiratory_rate, 'blood_pressure_systolic_sbs': bps_sbs, 'blood_pressure_systolic': 
-                                blood_pressure_systolic, 'blood_pressure_mean_sbs': bpm_sbs,'blood_pressure_mean': blood_pressure_mean, 
-                                'blood_pressure_diastolic_sbs': bpd_sbs,'blood_pressure_diastolic': blood_pressure_diastolic})
+            # Further processing and saving...
+            print('Save to file')
+            if windows:
+                windows_merged = reduce(lambda left, right: pd.merge(left, right, on=['dts'], how='outer'), windows)
+                windows_merged.drop('dts', axis=1, inplace=True)
+                windows_merged = windows_merged.apply(pd.to_numeric, downcast='float') # float32 is enough
+                windows_merged.interpolate(axis=1, inplace=True) # fill NA with linear interpolation           
+                windows_merged.dropna(inplace=True)  # Drop NaN values
 
-if __name__ == '__main__':       
+                filename = f'{patient}_SICKBAY_{lead_time}MIN_{window_size-lead_time}MIN.mat'
+                save_file = os.path.join(patient_dir, filename)
+                
+                savemat(save_file, {
+                    'heart_rate': windows_merged.filter(like='heart_rate').values.T,
+                    'SpO2': windows_merged.filter(like='SpO2').values.T,
+                    'respiratory_rate': windows_merged.filter(like='respiratory_rate').values.T,
+                    'blood_pressure_systolic': windows_merged.filter(like='blood_pressure_systolic').values.T,
+                    'blood_pressure_mean': windows_merged.filter(like='blood_pressure_mean').values.T,
+                    'blood_pressure_diastolic': windows_merged.filter(like='blood_pressure_diastolic').values.T,
+                    'sbs': sbs
+                })
+            else:
+                print("No data found for patient:", patient)
+
+if __name__ == '__main__':
     data_dir = r'C:\Users\jakes\Documents\DT 6 Analysis\PythonCode\PedAccel\data_analysis\PythonPipeline\PatientData'
-    #data_dir = r'C:\Users\sidha\OneDrive\Sid Stuff\PROJECTS\iMEDS Design Team\Data Analysis\PedAccel\data_analysis\PythonPipeline\PatientData'
     load_segment_sickbay(data_dir)
