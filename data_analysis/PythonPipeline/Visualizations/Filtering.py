0f5ca1b93ba532d8f1a7bd45db8e01e72f2b86a0
Jake <jakesamuelshoffmann@gmail.com>
2024-04-23 21:54:18 -0400

    Filtering working

--- data_analysis/PythonPipeline/Visualizations/Filtering.py
+++ data_analysis/PythonPipeline/Visualizations/Filtering.py
@@ -60,6 +60,8 @@ def interpolate_nan(signal, window_size):
             signal[nan_indices[-1] + 1:] = signal[nan_indices[-1]]
 
         return True  # Return True to indicate that interpolation/extrapolation was performed
+    else: 
+        return True; 
     
 
 #test
@@ -85,24 +87,37 @@ if __name__ == '__main__':
         accel_SBS = accel_data["sbs"]
         
         vitals_data = loadmat(data_filepath_vitals)
-        hr = vitals_data['heart_rate']
-        SpO2 = vitals_data['SpO2']
-        rr = vitals_data['respiratory_rate']
-        bps = vitals_data['blood_pressure_systolic']
-        bpm = vitals_data['blood_pressure_mean']
-        bpd = vitals_data['blood_pressure_diastolic']
-        vitals_SBS = vitals_data['sbs']
+        temp_hr = vitals_data['heart_rate']
+        temp_SpO2 = vitals_data['SpO2']
+        temp_rr = vitals_data['respiratory_rate']
+        temp_bps = vitals_data['blood_pressure_systolic']
+        temp_bpm = vitals_data['blood_pressure_mean']
+        temp_bpd = vitals_data['blood_pressure_diastolic']
+        vitals_SBS = vitals_data['sbs'].flatten()
+        hr = []
+        rr = []
+        SpO2 = []
+        bpm = []
+        bps = []
+        bpd = []
+
+        print(f'Before: {temp_bpd.shape} for {patient}')
+
         for j in range(len(vitals_SBS)):
-            if not checkVitals(hr[j], slice_size_min, 'hr'):
-                np.delete(hr,j)
-            if not checkVitals(rr[j], slice_size_min, 'rr'):
-                np.delete(rr, j)
-            if not checkVitals(SpO2[j], slice_size_min, 'spo2'):
-                np.delete(SpO2, j)
-            if not checkVitals(bps[j], slice_size_min, 'bps'):
-                 np.delete(bps, j)
-            if not checkVitals(bpm[j], slice_size_min, 'bpm'):
-                np.delete(bpm, j)
-            if not checkVitals(bpd[j], slice_size_min, 'bpd'):
-                np.delete(bpd, j)
-        print(bpd.shape)
+
+            if checkVitals(temp_hr[j], slice_size_min, 'hr'):
+                hr.append(temp_hr[j])
+            if checkVitals(temp_rr[j], slice_size_min, 'rr'):
+                rr.append(temp_hr[j])
+            if checkVitals(temp_SpO2[j], slice_size_min, 'spo2'):
+                SpO2.append(temp_hr[j])
+            if checkVitals(temp_bps[j], slice_size_min, 'bps'):
+                 bpm.append(temp_hr[j])
+            if checkVitals(temp_bpm[j], slice_size_min, 'bpm'):
+                bps.append(temp_hr[j])
+            if checkVitals(temp_bpd[j], slice_size_min, 'bpd'):
+                bpd.append(temp_hr[j])
+
+        #Call Functions for Analysis for each patient here!
+
+        print(f'After: {np.array(bpd).shape} for {patient}')
