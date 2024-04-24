50de9433e871397fad0286eb4b52e1aea7762162
Jake <jakesamuelshoffmann@gmail.com>
2024-04-23 20:56:26 -0400

    Valid range

--- data_analysis/PythonPipeline/Visualizations/Filtering.py
+++ data_analysis/PythonPipeline/Visualizations/Filtering.py
@@ -33,3 +33,31 @@ def interpolate_nan(signal, window_size):
             signal[nan_indices[-1] + 1:] = signal[nan_indices[-1]]      
               
         return True  # Return True to indicate that interpolation/extrapolation was performed
+    
+
+def checkVitals(vital_array, window_size, vital_type):
+    for index, val in enumerate(vital_array):
+        if vital_type == 'HR':
+            if val > 220 or val < 30:
+                vital_array[index] = np.NaN
+        elif vital_type == 'RR':
+            if val > 80 or val < 10:
+                vital_array[index] = np.NaN
+        elif vital_type == 'SpO2':
+            if val > 115 or val < 80:
+                vital_array[index] = np.NaN
+        elif vital_type == 'bps':
+            if val > 150 or val < 50:
+                vital_array[index] = np.NaN
+        elif vital_type == 'bpd':
+            if val > 90 or val < 30:
+                vital_array[index] = np.NaN
+        elif vital_type == 'bpm':
+            if val > 150 or val < 50:
+                vital_array[index] = np.NaN
+        else: 
+            print('Error, improper vital type specified')
+            return False
+    interpolate_nan(vital_array, window_size)
+    return True
+
